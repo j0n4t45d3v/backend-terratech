06f6334604fbe5b77fb08ae41ca2865d1d884af3
Jonatas <delimajonatas875@gmail.com>
2023-03-03 09:36:57 -0300

    Initial commit

---
+++ src/main/java/br/com/terratech/backendterratech/BackendTerratechApplication.java
@@ -0,0 +1,13 @@
+package br.com.terratech.backendterratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTerratechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTerratechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/terratech/backendterratech/BackendTerratechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.terratech.backendterratech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTerratechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
