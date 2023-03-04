df4e4c8a9a5a75a5a0d4cc4b952c5995ea88b7fa
Jonatas <delimajonatas875@gmail.com>
2023-03-03 21:27:54 -0300

    service product

--- src/main/java/br/com/terratech/backendterratech/services/UserService.java
+++ src/main/java/br/com/terratech/backendterratech/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -28,5 +29,13 @@ public class UserService {
 
   public List<User> findAllUsers() {
     return userRepository.findAll();
+  }
+
+  public void deleteUser(String cpf){
+      userRepository.deleteById(cpf);
+  }
+
+  public void updateUser(String cpf){
+      Optional<User> user = userRepository.findById(cpf);
   }
 }
