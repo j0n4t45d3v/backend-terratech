0d38089541754774d3ed9f1344bd488c7b78eeb4
Jonatas <delimajonatas875@gmail.com>
2023-03-08 13:06:56 -0300

    Dockerfile

--- src/main/java/br/com/terratech/backendterratech/BackendTerratechApplication.java
+++ src/main/java/br/com/terratech/backendterratech/BackendTerratechApplication.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BackendTerratechApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendTerratechApplication.class, args);
