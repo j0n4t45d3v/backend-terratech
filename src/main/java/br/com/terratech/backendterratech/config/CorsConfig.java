91c179f52cbb14af22b50429c1c00db6e3dabc3d
Jonatas <delimajonatas875@gmail.com>
2023-03-10 11:32:54 -0300

    ajustes

--- src/main/java/br/com/terratech/backendterratech/config/CorsConfig.java
+++ src/main/java/br/com/terratech/backendterratech/config/CorsConfig.java
@@ -6,18 +6,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000/")
+            .allowedOrigins("http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(false);
 
-    //registry.addMapping("/**")
-    //        .allowedOrigins("https://terratech.vercel.app/")
-    //        .allowedMethods("GET", "POST", "PUT", "DELETE")
-    //        .allowCredentials(false);
+    registry.addMapping("/**")
+            .allowedOrigins("https://terratech.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(false);
   }
 }
